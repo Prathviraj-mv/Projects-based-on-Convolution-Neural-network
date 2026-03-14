c06901574899ff0a840b44c95aca45ba55728978
chappie <168354749+Prathviraj-mv@users.noreply.github.com>
2026-03-14 09:46:03 +0530

    Create run.py

---
+++ YOLO_DRONE n8/run.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("runs/detect/train/weights/best.pt")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    results = model(frame)
+
+    annotated = results[0].plot()
+
+    cv2.imshow("Drone Detection", annotated)
+
+    if cv2.waitKey(1) == 27:
+        break
